0ddd45cc432f87091f4af8dac5aa39b3ddec281a
Jin Park <jiny199109@gmail.com>
2013-05-31 19:15:30 +0900

    이중 스크롤

---
+++ sample/src/com/viewpagerindicator/sample/UninterceptableViewPager.java
@@ -0,0 +1,25 @@
+package com.viewpagerindicator.sample;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by jin on 13. 5. 31..
+ */
+public class UninterceptableViewPager extends ViewPager {
+
+    public UninterceptableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        // Tell our parent to stop intercepting our events!
+        boolean ret = super.onInterceptTouchEvent(ev);
+        if (ret) {
+            getParent().requestDisallowInterceptTouchEvent(true);
+        }
+        return ret;
+    }
+}

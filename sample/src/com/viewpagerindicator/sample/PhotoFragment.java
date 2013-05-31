5263f74e028242a62cb146d0dcfb276fbe26a966
Jin Park <jiny199109@gmail.com>
2013-05-31 22:06:53 +0900

    image fragment

---
+++ sample/src/com/viewpagerindicator/sample/PhotoFragment.java
@@ -0,0 +1,34 @@
+package com.viewpagerindicator.sample;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import uk.co.senab.photoview.PhotoView;
+
+/**
+ * Created by jin on 13. 5. 31..
+ */
+public class PhotoFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        PhotoView image = new PhotoView(getActivity());
+        image.setImageResource(R.drawable.wallpaper);
+        image.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, Gravity.CENTER));
+        return image;
+    }
+}
--- sample/src/com/viewpagerindicator/sample/SampleCirclesDefault.java
+++ sample/src/com/viewpagerindicator/sample/SampleCirclesDefault.java
@@ -1,21 +1,54 @@
 package com.viewpagerindicator.sample;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
 import com.viewpagerindicator.CirclePageIndicator;
+import uk.co.senab.photoview.PhotoView;
 
 public class SampleCirclesDefault extends BaseSampleActivity {
+
+    SamplePagerAdapter mPagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.simple_circles);
 
-        mAdapter = new TestFragmentAdapter(getSupportFragmentManager());
+        mPagerAdapter = new SamplePagerAdapter(getSupportFragmentManager());
 
         mPager = (ViewPager)findViewById(R.id.pager);
-        mPager.setAdapter(mAdapter);
+        mPager.setAdapter(mPagerAdapter);
 
         mIndicator = (CirclePageIndicator)findViewById(R.id.indicator);
         mIndicator.setViewPager(mPager);
     }
+
+    static class SamplePagerAdapter extends FragmentPagerAdapter {
+    private int mCount = 6;
+
+        public SamplePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return mCount;
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return new PhotoFragment();
+        }
+
+
+
+    }
+
+
 }
